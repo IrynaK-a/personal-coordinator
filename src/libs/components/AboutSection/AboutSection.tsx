19a82100fae78fd0281903625d53b117d084d2b7
Iryna Konovalova <108731612+IrynaK-a@users.noreply.github.com>
2024-05-17 17:02:36 +0200

    update AboutSection

--- src/libs/components/AboutSection/AboutSection.tsx
+++ src/libs/components/AboutSection/AboutSection.tsx
@@ -21,14 +21,13 @@ export const AboutSection: React.FC<Props> = ({ className }) => {
 
         <p className={styles.aboutText}>
           <span className={styles.styledName}>{`Personal coordinator `} </span>
-          is an innovating e-learning platform committed to ensuring accessible
-          and effective education. Our platform is created to upgarate your
-          studying experience and create a friendly and modern environment for
-          studying
+          is an innovative e-learning platform that ensures accessible and effective education.
+          Our platform is created to upgrade your studying experience and create a friendly and 
+          modern study environment.
         </p>
 
         <h2 className={styles.subtitle}>
-          <span className={styles.styledSubtitleText}>Experience a</span> better
+          <span className={styles.styledSubtitleText}>Experience </span> better
           learning
           <span className={styles.styledSubtitleText}> with AI</span>
         </h2>
